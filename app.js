f11ed265b776906420c3e91f15702e462fe1e9da
Jonathan Rayback <jonathan.rayback@evernym.com>
2023-04-12 16:07:50 -0600

    refactor

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require('path')
-
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -7,11 +6,11 @@ const errorController = require('./controllers/error')
 
 const app = express()
 
-app.set('view engine', 'pug')
-app.set('views', 'views')
-
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
