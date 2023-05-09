9e4e506e82fc64a7b945b344755a9c12aa4d1ab2
Jonathan Rayback <jonathan.rayback@evernym.com>
2023-05-09 14:13:07 -0600

    remove comments in cli app file

--- adapters/primary/cli/app.js
+++ adapters/primary/cli/app.js
@@ -7,7 +7,6 @@ const addProduct = require('../../../application/use-cases/addProduct')
 const getProducts = require('../../../application/use-cases/getProducts')
 const productRepo = require('../../secondary/fileSystemDataRepo/productRepo')
 
-// I'm pretty sure the problem is somewhere down here ... there's some sort of callback synchronization issue happening I think.
 yargs(hideBin(process.argv))
   .command('add-product [title]', 'Add a product', (yargs) => {
     return yargs
